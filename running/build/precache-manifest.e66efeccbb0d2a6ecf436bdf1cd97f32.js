3627493e20a82d3eb4c5d1daa80f9a4e04a5622b
zackSolidd <zacktjc@gmail.com>
2020-09-03 17:02:50 +0800

    Add built files

---
+++ running/build/precache-manifest.e66efeccbb0d2a6ecf436bdf1cd97f32.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16aa621ab12d8b0eb579e463fdde21bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3b9d4f3bd11ade71325b",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "36c73b5068e69bd47c2e",
+    "url": "/static/css/main.f0affe58.chunk.css"
+  },
+  {
+    "revision": "3b9d4f3bd11ade71325b",
+    "url": "/static/js/2.a2eedcb6.chunk.js"
+  },
+  {
+    "revision": "08e82431ad6036365c5a5a461dbc5cb1",
+    "url": "/static/js/2.a2eedcb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "36c73b5068e69bd47c2e",
+    "url": "/static/js/main.b0f39d55.chunk.js"
+  },
+  {
+    "revision": "5e4407fa3c504d226775",
+    "url": "/static/js/runtime-main.1e5016ed.js"
+  }
+]);
