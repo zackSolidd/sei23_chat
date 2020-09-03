8090fc4b57b02b1effd9a5f8d5e748e2501e137d
zackSolidd <zacktjc@gmail.com>
2020-09-03 16:17:47 +0800

    Deployment run

--- index.js
+++ index.js
@@ -2,14 +2,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-var cors = require('cors');
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
-
+const path = require("path");
+var cors = require("cors");
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
 //=== add all middlewares
 app.use(cors());
-    /* 
+/* 
     ===================
     Connect to MongoDB 
     */
@@ -20,28 +20,26 @@ app.use(express.json()); //allows me to receive JSON files from HEADER of REQUES
 app.use("/api/auth", require("./routes/auth.route"));
 app.use("/api/chatroom", require("./routes/chatRoom.route"));
 app.use("/api/globalchatroom", require("./routes/globalchat.route"));
+app.use(express.static(path.join(__dirname, "client", "build")));
 
-
-
-// socket.io 
-io.on('connection', async (socket) => {
-
-  socket.on('message', ({username,message})=> {
-    io.emit('message', {username, message})
-  })
-  
+// socket.io
+io.on("connection", async (socket) => {
+  socket.on("message", ({ username, message }) => {
+    io.emit("message", { username, message });
+  });
 });
 
-
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   req.io = io;
   next();
 });
 
 //=== 404 errors
+
 app.get("*", (req, res) => {
-    res.status(404).json({ message: "E404 : Page not found", code: "EB404" });
-  });
+  res.sendFile(path.join(__dirname, "running", "build", "index.html"));
+});
+
 //=== setup the server port
 http.listen(process.env.PORT, () =>
   console.log(`running on ${process.env.PORT}`)
